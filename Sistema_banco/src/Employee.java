a65d5b73cb99ff128c7e0d4c13588346dcb3e2ea
João Victor <jvbernardes420@gmail.com>
2024-04-27 16:17:28 -0300

    Adicionando thread de funcionario e seus metodos

--- Sistema_banco/src/Employee.java
+++ Sistema_banco/src/Employee.java
@@ -1,2 +1,24 @@
-public class Employee {
+public class Employee extends Thread {
+    // criando o salario visivel para todas as classes
+    public static final double salary = 1400.0;
+    private Account salaryAccount;
+    public Account investmentAccount;
+
+    public Employee(Account salaryAccount, Account investmentAccount) {
+        this.salaryAccount = salaryAccount;
+        this.investmentAccount = investmentAccount;
+    }
+
+    // gerar o get para conseguir visualizar o salario e poder manipula-lo
+    public Account getSalaryAccount() {
+        return salaryAccount;
+    }
+
+    public void investir() {
+        double investmentAmount = salary * 0.2;
+        salaryAccount.transfer(investmentAccount, investmentAmount);
+        System.out.println(Thread.currentThread().getName() + " investiu R$" + investmentAmount + "(20% do total) na conta de investimentos.");
+    }
+
+
 }
